06d276e967c4bf9d075062077823492537c44346
Kauã Felipe <89869280+kauafelipe98@users.noreply.github.com>
2024-07-23 10:43:20 -0300

    Update utils.py

--- Project/utils.py
+++ Project/utils.py
@@ -8,7 +8,7 @@ openai.api_key = st.secrets["api_keys"]["openai_api_key"]
 
 
 def create_assistant(file_path, new_message, thread_id=None):
-    client = OpenAI()
+    client = OpenAI(api_key=openai.api_key)
 
     assistant_id = "asst_hXtWb0KJFKluwl2jJr0ov5tZ"
 
